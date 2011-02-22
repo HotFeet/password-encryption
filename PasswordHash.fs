bdbd10d25c3aabbf117a570ed1d3e3e36310b99b
Juraj Skripsky <juraj.skripsky@gmail.com>
2011-02-22 02:27:49 +0100

    Steady progress.

--- PasswordHash.fs
+++ PasswordHash.fs
@@ -12,15 +12,27 @@ let hashBitLen = 512
 let base64Len bitLen = (bitLen + 5) / 6
 let bytes (s: string) = Encoding.UTF8.GetBytes(s)
 
-let base64Str (bytes: byte[]) = "Test"
-let base64Bytes (s: string) = [|0uy|]
+let fromBase64 = Convert.FromBase64String
+let toBase64 = Convert.ToBase64String
+
+let base64Str (bytes : byte[]) =
+	let extend bytes = Array.append bytes [|0uy; 0uy|]
+	let trim (str: string) = str.Substring(0, bytes.Length)
+	match (bytes.Length) with
+	| len when (len % 3 = 0) -> bytes |> toBase64
+	| len -> bytes |> extend |> toBase64 |> trim
+
+let base64Bytes (str : string) =
+	match (str.Length * 6) with
+	| len when (len % 8 = 0) -> Convert.FromBase64String(str)
+	| len -> Convert.FromBase64String(str + "AA").[0..(len >>> 3)]
 
 let formatHash salt hash = sprintf "$6$%s$%s" (base64Str salt) (base64Str hash) 
 let base64Group bitLen = sprintf "(.{%d})" (bitLen |> base64Len)
 let hashPattern = sprintf "\$6\$%s\$%s" (base64Group saltBitLen) (base64Group hashBitLen) 
 let hashRegex = new Regex(hashPattern)
 		
-let base64Zero = Convert.ToBase64String([|0uy; 0uy; 0uy|]).[0]
+let base64Zero = 'A'
 
 let hashAlgo = HashAlgorithm.Create("SHA512")
 let hashBytes bytes =
@@ -34,37 +46,6 @@ let random len =
 	let bytes = byte[len]
 	rangGen.GetBytes(bytes)
 	bytes
-
-(*
-	static string ToFilledBase64(byte[] bytes) {
-		int mod = bytes.Length % 3;
-		if(mod == 0)
-			return Convert.ToBase64String(bytes);
-	
-		int strLen = (bytes.Length * 8 + 5) / 6;
-		int excess = 3 - mod;
-		byte[] paddedBytes = new byte[bytes.Length + excess];
-		Buffer.BlockCopy(bytes, 0, paddedBytes, 0, bytes.Length);
-		for(int i = 0; i < excess; i++)
-			paddedBytes[bytes.Length + i] = 0;
-	 
-		return Convert.ToBase64String(paddedBytes).Substring(0, strLen);
-	}
-	
-	static byte[] FromFilledBase64(string s) {
-		int filledBits = s.Length * 6;
-		if(filledBits % 8 == 0)
-			return Convert.FromBase64String(s);
-	
-		//FIXME: don't hardcode the count
-		s += new String(zeroChar, 2);
-		byte[] bytesExcessive = Convert.FromBase64String(s);
-		byte[] bytes = new byte[filledBits >> 3];
-		Array.Copy(bytesExcessive, bytes, bytes.Length);
-	
-		return bytes;
-	}
-*)
 
 let Hash password =
 	let salt = random (saltBitLength >> 3)
