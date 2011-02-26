a00d1845231458b0a953e7732d53d5cc5dd347d8
Juraj Skripsky <juraj.skripsky@gmail.com>
2011-02-27 00:52:42 +0100

    Switch back to Convert.{From,To}Base64String.

--- Base64.fs
+++ Base64.fs
@@ -21,26 +21,23 @@
 
 open System
 open System.Text
-open System.Security.Cryptography
 
 namespace HotFeet.Text
 	module Base64 =
-		let trim len (bs: byte[]) = bs.[0..(len - 1)]
-		let pad value len (bs : byte[]) =
-			match len with
-			| 0 -> bs
-			| _ -> Array.append bs (Array.create len value)
-	
-		let transform (ct : ICryptoTransform) (bs : byte[]) =
-			let (ibs, obs) = (ct.InputBlockSize, ct.OutputBlockSize)
-			let res = Array.zeroCreate ((bs.Length * obs + ibs - 1) / ibs)
-			let mutable (srcIdx, dstIdx) = (0, 0)
-			while bs.Length - srcIdx > ibs do
-				let _ = ct.TransformBlock(bs, srcIdx, ibs, res, dstIdx)
-				srcIdx <- srcIdx + ibs
-				dstIdx <- dstIdx + obs
-			Array.append (trim dstIdx res) (ct.TransformFinalBlock(bs, srcIdx, bs.Length - srcIdx))
+		let fromBase64 = Convert.FromBase64String
+		let toBase64 = Convert.ToBase64String
 		
+		let public toString (bs : byte[]) =
+			let appendZero count bs = Array.append bs (Array.zeroCreate count)
+			let trim (str: string) = str.Substring(0, base64Len (bs.Length * 8))
+			match (bs.Length) with
+			| len when (len % 3 = 0) -> bs |> toBase64
+			| _ -> bs |> appendZero 2 |> toBase64 |> trim
+		
+		let public zero = 'A'
+		let public ofString (s : string) =
+			match (s.Length * 6) with
+			| len when (len % 8 = 0) -> s |> fromBase64
+			| len -> ((str + "AA") |> fromBase64).[0..((len >>> 3) - 1)]
+
 		let public getStringLen (bitLen : int) = (bitLen + 5) / 6
-		let public toString (s: string) = s |> toBytes |> transform (new FromBase64Transform())
-		let public ofString (bs: byte[]) = bs |> transform (new ToBase64Transform()) |> trim (GetByteLen (bs.Length * 8)) |> toString
